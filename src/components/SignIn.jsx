02ee95e6ab183de2f3712ec0e2d83a9b54142d3c
Reniel Ocampo <renielxocampo@gmail.com>
2021-10-21 21:09:00 -0700

    fill sign in view using formik

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
-import theme from '../theme';
 import Text from './Text';
+import { Pressable, View, StyleSheet } from 'react-native';
+import FormikTextInput from './FormikTextInput';
+import { Formik } from 'formik';
 
 const SignIn = () => {
   const styles = {
-    signIn: {
-      color: theme.colors.textPrimary
+    wrapper: {
+      padding: 10,
     },
   };
 
-  return <Text style={styles.signIn}>The sign in view</Text>;
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.signIn}>The sign in view</Text>
+      <Authentication />
+    </View>
+  );
+};
+
+const Form = ({ onSubmit }) => {
+  const styles = StyleSheet.create({
+    field: {
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 20,
+    }
+  });
+
+  return (
+    <>
+      <Text>Username</Text>
+      <FormikTextInput name='username' placeholder='Username' style={styles.field} />
+      <Text>Password</Text>
+      <FormikTextInput name='password' placeholder='Password' secureTextEntry style={styles.field} />
+      <Pressable onPress={onSubmit}>
+        <Text>Sign In</Text>
+      </Pressable>
+    </>
+  );
+};
+
+
+const Authentication = () => {
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <Form onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  error: {
+    borderWidth: 1,
+    borderColor: 'red',
+  }
+});
+
+const TextInput = ({ style, error, ...props }) => {
+  const textInputStyle = [
+    style,
+    error && styles.error,
+  ];
+
+  return <NativeTextInput style={textInputStyle} {...props} />;
+};
+
+export default TextInput;
